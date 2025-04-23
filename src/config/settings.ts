8e4434e4c8dacd4ad4e6efd07701d17d7d576312
ChinmayPPP <chinmay@pinegap.us>
2025-04-24 02:47:52 +0530

    handeled env undefined error

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -3,14 +3,14 @@ import dataSource from "~src/config/ormconfig.pgap";
 import { Config } from "./types";
 
 export const conf: Config = {
-    DB_NAME: process.env.DB_NAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_PORT: process.env.DB_PORT,
-    DB_USER: process.env.DB_USER,
-    DB_HOST: process.env.DB_HOST,
-    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
-    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
-    REGION: process.env.REGION,
-    PORT: process.env.PORT,
+    DB_NAME: process.env.DB_NAME!,
+    DB_PASSWORD: process.env.DB_PASSWORD!,
+    DB_PORT: process.env.DB_PORT!,
+    DB_USER: process.env.DB_USER!,
+    DB_HOST: process.env.DB_HOST!,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY!,
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID!,
+    REGION: process.env.REGION!,
+    PORT: process.env.PORT!,
     DEFAULT_DATA_SOURCE: dataSource,
 };
